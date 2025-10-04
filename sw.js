298ee869dd41153803d53d238e0eb7b908a719f5
danielroufe <daniel.roufe@gmail.com>
2025-10-04 10:26:40 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,31 +1,11 @@
-const CACHE = 'kidroutine-v1';
-const ASSETS = [
-  './',
-  './index.html',
-  './manifest.webmanifest',
-  './icons/icon-192.png',
-  './icons/icon-512.png'
-];
-
-self.addEventListener('install', (event) => {
-  event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS)));
-  self.skipWaiting();
-});
-
-self.addEventListener('activate', (event) => {
-  event.waitUntil(
-    caches.keys().then(keys => Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))))
-  self.clients.claim();
-});
-
-self.addEventListener('fetch', (event) => {
-  const req = event.request;
-  if(req.method !== 'GET'){ return; }
-  event.respondWith(
-    caches.match(req).then(cached => cached || fetch(req).then(res => {
-      const copy = res.clone();
-      caches.open(CACHE).then(cache => cache.put(req, copy)).catch(()=>{});
-      return res;
-    }).catch(()=>caches.match('./')))
-  );
+const CACHE = 'kidroutine-deluxe-voice-v1';
+const ASSETS = ['./','./index.html','./manifest.webmanifest','./icons/icon-192.png','./icons/icon-512.png'];
+self.addEventListener('install', e => { e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))); self.skipWaiting(); });
+self.addEventListener('activate', e => { e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))); self.clients.claim(); });
+self.addEventListener('fetch', e => {
+  if(e.request.method!=='GET') return;
+  e.respondWith(caches.match(e.request).then(c => c || fetch(e.request).then(net => {
+    const cp = net.clone(); caches.open(CACHE).then(cc=>cc.put(e.request, cp));
+    return net;
+  }).catch(()=>caches.match('./'))));
 });
